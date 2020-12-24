aa6285e516aa72cec928f4546cfd99bbb51a01ed
Kari Magnusson <karimagnusson@yandex.com>
2020-12-24 21:35:59 +0300

    update tested

--- model/filter/Modification.scala
+++ model/filter/Modification.scala
@@ -9,15 +9,15 @@ trait Modification extends ModelRender {
 }
 
 case class SetValue(col: ModelCol, arg: Any) extends Modification {
-  def render = "${col.name} = ?"
+  def render = s"${col.name} = ?"
 }
 
 case class Increment(col: ModelCol, arg: Any) extends Modification {
-  def render = "${col.name} = ${col.name} + ?"
+  def render = s"${col.name} = ${col.name} + ?"
 }
 
 case class Decrement(col: ModelCol, arg: Any) extends Modification {
-  def render = "${col.name} = ${col.name} - ?"
+  def render = s"${col.name} = ${col.name} - ?"
 }
 
 
