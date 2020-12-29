615bd98dbaf565de1793418aa8781ebba3f8c698
Kari Magnusson <karimagnusson@yandex.com>
2020-12-29 22:04:03 +0300

    col typed included

--- model/ModelCol.scala
+++ model/ModelCol.scala
@@ -17,7 +17,7 @@ trait ModelCol extends Render {
 
   def render = {
     model.__prefix match {
-      case Some(prefix) => wrap("%s.%s".format(prefix, name))
+      case Some(prefix) => "%s.%s".format(wrap(prefix), wrap(name))
       case None => wrap(name) 
     }
   }
@@ -35,7 +35,8 @@ case class StringCol(name: String, model: Model) extends TypeCol[String]
 
   def get(row: Row): String = row.str(name)
 }
-    
+
+
 case class IntCol(name: String, model: Model) extends TypeCol[Int]
                                                  with UniversalFilters[Int]
                                                  with ComparativeFilters[Int]
@@ -43,73 +44,113 @@ case class IntCol(name: String, model: Model) extends TypeCol[Int]
   def get(row: Row): Int = row.int(name)
 }
 
+
 case class BooleanCol(name: String, model: Model) extends TypeCol[Boolean] 
                                                      with UniversalFilters[Boolean]{
 
   def get(row: Row): Boolean = row.bool(name)
 }
 
-/*
-case class CharCol(name: String)(implicit model: Model) extends ModelColumn[Char] {
+
+case class CharCol(name: String, model: Model) extends TypeCol[Char]
+                                                           with UniversalFilters[Char] {
 
   def get(row: Row): Char = row.char(name)
 }
 
-case class ShortCol(name: String)(implicit model: Model) extends ModelColumn[Short] {
+
+case class ShortCol(name: String, model: Model) extends TypeCol[Short]
+                                                            with UniversalFilters[Short]
+                                                            with ComparativeFilters[Short]
+                                                            with IncrementUpdate[Short] {
 
   def get(row: Row): Short = row.short(name)
 }
 
-case class LongCol(name: String)(implicit model: Model) extends ModelColumn[Long] {
+
+case class LongCol(name: String, model: Model) extends TypeCol[Long]
+                                                           with UniversalFilters[Long]
+                                                           with ComparativeFilters[Long]
+                                                           with IncrementUpdate[Long] {
 
   def get(row: Row): Long = row.long(name)
 }
 
-case class BigDecimalCol(name: String)(implicit model: Model) extends ModelColumn[BigDecimal] {
+
+case class BigDecimalCol(name: String, model: Model) extends TypeCol[BigDecimal]
+                                                                 with UniversalFilters[BigDecimal]
+                                                                 with ComparativeFilters[BigDecimal]
+                                                                 with IncrementUpdate[BigDecimal] {
 
   def get(row: Row): BigDecimal = row.bigDecimal(name)
 }
 
-case class DecimalNumberCol(name: String)(implicit model: Model) extends ModelColumn[DecimalNumber] {
+
+case class DecimalNumberCol(name: String, model: Model) extends TypeCol[DecimalNumber]
+                                                                    with UniversalFilters[DecimalNumber]
+                                                                    with ComparativeFilters[DecimalNumber]
+                                                                    with IncrementUpdate[DecimalNumber] {
 
   def get(row: Row): DecimalNumber = row.decimal(name)
 }
 
-case class DoubleCol(name: String)(implicit model: Model) extends ModelColumn[Double] {
+
+case class DoubleCol(name: String, model: Model) extends TypeCol[Double]
+                                                             with UniversalFilters[Double]
+                                                             with ComparativeFilters[Double]
+                                                             with IncrementUpdate[Double] {
 
   def get(row: Row): Double = row.double(name)
 }
 
-case class FloatCol(name: String)(implicit model: Model) extends ModelColumn[Float] {
+
+case class FloatCol(name: String, model: Model) extends TypeCol[Float]
+                                                            with UniversalFilters[Float]
+                                                            with ComparativeFilters[Float]
+                                                            with IncrementUpdate[Float] {
 
   def get(row: Row): Float = row.float(name)
 }
 
-case class InstantCol(name: String)(implicit model: Model) extends ModelColumn[Instant] {
+
+case class InstantCol(name: String, model: Model) extends TypeCol[Instant]
+                                                              with UniversalFilters[Instant] {
 
   def get(row: Row): Instant = row.instant(name)
 }
 
-case class ZonedDateTimeCol(name: String)(implicit model: Model) extends ModelColumn[ZonedDateTime] {
+
+case class ZonedDateTimeCol(name: String, model: Model) extends TypeCol[ZonedDateTime]
+                                                                    with UniversalFilters[ZonedDateTime]
+                                                                    with ComparativeFilters[ZonedDateTime] {
 
   def get(row: Row): ZonedDateTime = row.zonedDateTime(name)
 }
 
-case class LocalDateTimeCol(name: String)(implicit model: Model) extends ModelColumn[LocalDateTime] {
+
+case class LocalDateTimeCol(name: String, model: Model) extends TypeCol[LocalDateTime]
+                                                                    with UniversalFilters[LocalDateTime]
+                                                                    with ComparativeFilters[LocalDateTime] {
 
   def get(row: Row): LocalDateTime = row.localDateTime(name)
 }
 
-case class LocalDateCol(name: String)(implicit model: Model) extends ModelColumn[LocalDate] {
+
+case class LocalDateCol(name: String, model: Model) extends TypeCol[LocalDate]
+                                                                with UniversalFilters[LocalDate]
+                                                                with ComparativeFilters[LocalDate] {
 
   def get(row: Row): LocalDate = row.localDate(name)
 }
 
-case class UUIDCol(name: String)(implicit model: Model) extends ModelColumn[UUID] {
+
+case class UUIDCol(name: String, model: Model) extends TypeCol[UUID]
+                                                           with UniversalFilters[UUID]
+                                                           with ComparativeFilters[UUID] {
 
   def get(row: Row): UUID = row.uuid(name)
 }
-*/
+
 
 
 
--- model/implicits.scala
+++ model/implicits.scala
@@ -10,11 +10,17 @@ object implicits {
   implicit val implColString: ColConf => TypeCol[String] = c => StringCol(c.name, c.model)
   implicit val implColInt: ColConf => TypeCol[Int] = c => IntCol(c.name, c.model)
   implicit val implColBoolean: ColConf => TypeCol[Boolean] = c => BooleanCol(c.name, c.model)
+  implicit val implColChar: ColConf => TypeCol[Char] = c => CharCol(c.name, c.model)
+  implicit val implColShort: ColConf => TypeCol[Short] = c => ShortCol(c.name, c.model)
+  implicit val implColLong: ColConf => TypeCol[Long] = c => LongCol(c.name, c.model)
 
   // filters
   implicit val modelColToStringCol: TypeCol[String] => StringCol = col => col.asInstanceOf[StringCol]
   implicit val modelColToIntCol: TypeCol[Int] => IntCol = col => col.asInstanceOf[IntCol]
   implicit val modelColToBooleanCol: TypeCol[Boolean] => BooleanCol = col => col.asInstanceOf[BooleanCol]
+  implicit val modelColToCharCol: TypeCol[Char] => BooleanCol = col => col.asInstanceOf[CharCol]
+  implicit val modelColToShortCol: TypeCol[Short] => ShortCol = col => col.asInstanceOf[ShortCol]
+  implicit val modelColToLongCol: TypeCol[Long] => LongCol = col => col.asInstanceOf[LongCol]
 
   // query
   implicit val modelColToSorting: ModelCol => Sorting = col => Sort(col)
