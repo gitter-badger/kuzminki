8ff842abfd4ee94add05e675ba3308ff6308a6b8
Kari Magnusson <karimagnusson@yandex.com>
2021-06-30 22:38:46 +0600

    insert subquery readded

--- insert/RunInsert.scala
+++ insert/RunInsert.scala
@@ -25,7 +25,8 @@ class RunInsert[M <: Model, P](
       protected val coll: InsertCollector[P]
     ) extends PickInsertReturning[M, P]
          with WhereNotExists[M, P]
-         with OnConflict[M, P] {
+         with OnConflict[M, P]
+         with InsertSubquery[M, P] {
 
   def cache = coll.add(InsertBlankValuesSec(coll.paramShape.size)).cacheInsert
 
